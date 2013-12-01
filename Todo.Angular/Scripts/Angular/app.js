c1ac5bbaad71580fa090b5db4cd2b1a088838def
meilke <christian.meilke@googlemail.com>
2013-12-01 21:09:01 +0100

    Added dynamic menu

---
+++ Todo.Angular/Scripts/Angular/app.js
@@ -0,0 +1,49 @@
+﻿
+function Service() {
+
+    this.addEntries = angular.bind(this, this.addEntry);
+    this.registerObserverCallback = angular.bind(this, this.registerObserverCallback);
+
+}
+
+Service.prototype = {
+
+    observerCallbacks: [],
+    registerObserverCallback: function (callback) {
+        this.observerCallbacks.push(callback);
+    },
+    notifyObservers: function () {
+        var e = this.entries;
+        angular.forEach(this.observerCallbacks, function (callback) {
+            callback(e);
+        });
+    },
+
+    entries: [],
+    addEntry: function (title) {
+        this.entries.push(title);
+        this.notifyObservers();
+    }
+};
+
+angular.element(document).ready(function () {
+
+    angular
+        .module('Components', [])
+        .factory('MenuService', function () {
+            return new Service();
+        });
+
+    angular
+        .module('Menu', ['Components'])
+        .controller('MenuController', ['$scope', 'MenuService', function ($scope, MenuService) {
+
+            MenuService.registerObserverCallback(function (entries) {
+                $scope.menus = entries;
+                $scope.$apply();
+            });
+
+        }]);
+
+    angular.bootstrap($('#menu').get()[0], ['Menu']);
+});
---
+++ Todo.Angular/Scripts/Angular/inner-app.js
@@ -0,0 +1,38 @@
+﻿angular.element(document).ready(function () {
+
+    angular
+        .module('App', ['Components'])
+        .controller('InnerAppController', ['$scope', 'MenuService', function ($scope, MenuService) {
+
+            $scope.todos = [
+                
+            ];
+
+            $scope.addTodo = function () {
+                $scope.todos.push({ text: $scope.todoText, done: false });
+
+                MenuService.addEntry($scope.todoText);
+
+                $scope.todoText = '';
+            };
+
+            $scope.remaining = function () {
+                var count = 0;
+                angular.forEach($scope.todos, function (todo) {
+                    count += todo.done ? 0 : 1;
+                });
+                return count;
+            };
+
+            $scope.archive = function () {
+                var oldTodos = $scope.todos;
+                $scope.todos = [];
+                angular.forEach(oldTodos, function (todo) {
+                    if (!todo.done) $scope.todos.push(todo);
+                });
+            };
+        }]);
+
+    angular.bootstrap($('#inner').get()[0], ['App']);
+});
+
