a103181fc8152014121221e797c5c47ea57870ce
meilke <christian.meilke@googlemail.com>
2013-12-08 16:48:24 +0100

    Controller as class

--- Todo.Angular/Scripts/Angular/inner-app.js
+++ Todo.Angular/Scripts/Angular/inner-app.js
@@ -2,78 +2,7 @@
 
     angular
         .module('App', ['Components'])
-        .controller('InnerAppController', ['$scope', '$filter', 'MenuService', function ($scope, $filter, MenuService) {
-
-            MenuService.registerMenuChooseCallback(function (category) {
-                $scope.category = category;
-
-                var todosFromCategory = $filter('filter')($scope.todos, { categoryId: $scope.category.id });
-                $scope.currentTodos = todosFromCategory;
-
-                $scope.$apply();
-            });
-
-            $scope.categories = [
-                {id: 1, name: 'First Category', todoCount: 1}
-            ];
-
-            MenuService.updateEntries($scope.categories);
-
-            $scope.todos = [
-                { id: 1, text: 'First Task', categoryId: 1, done: false }
-            ];
-
-            $scope.category = $scope.categories[0];
-
-            var todosFromCategory = $filter('filter')($scope.todos, { categoryId: $scope.category.id });
-            $scope.currentTodos = todosFromCategory;
-
-            $scope.addTodo = function () {
-                $scope.todos.push({ text: $scope.todoText, done: false, categoryId: $scope.category.id });
-
-                var todosFromCategory = $filter('filter')($scope.todos, { categoryId: $scope.category.id });
-                var todosFromCategoryCount = todosFromCategory.length;
-
-                $scope.category.todoCount = todosFromCategoryCount;
-                $scope.todoText = '';
-
-                $scope.currentTodos = todosFromCategory;
-
-                MenuService.updateEntries($scope.categories);
-            };
-
-            $scope.addCategory = function () {
-
-                var sortedCategories = $filter('filter')($scope.categories, function (category) { return category.id; });
-                var newCategory = { id: sortedCategories[sortedCategories.length - 1].id + 1, name: $scope.categoryText, todoCount: 0 }
-
-                $scope.categories.push(newCategory);
-                $scope.category = newCategory;
-
-                var todosFromCategory = $filter('filter')($scope.todos, { categoryId: $scope.category.id });
-                $scope.currentTodos = todosFromCategory;
-
-                MenuService.updateEntries($scope.categories);
-
-                $scope.categoryText = '';
-            };
-
-            $scope.remaining = function () {
-                var count = 0;
-                angular.forEach($scope.currentTodos, function (todo) {
-                    count += todo.done ? 0 : 1;
-                });
-                return count;
-            };
-
-            $scope.archive = function () {
-                var oldTodos = $scope.todos;
-                $scope.todos = [];
-                angular.forEach(oldTodos, function (todo) {
-                    if (!todo.done) $scope.todos.push(todo);
-                });
-            };
-        }]);
+        .controller('InnerAppController', ['$scope', '$filter', 'MenuService', InnerAppController]);
 
     angular.bootstrap($('#inner').get()[0], ['App']);
 });
---
+++ Todo.Angular/Scripts/Angular/innerAppController.js
@@ -0,0 +1,88 @@
+﻿function InnerAppController($scope, $filter, MenuService) {
+
+    this.scope = $scope;
+    this.filter = $filter;
+    this.menuService = MenuService;
+
+    this.init();
+
+    this.scope.addTodo = angular.bind(this, this.addTodo);
+    this.scope.addCategory = angular.bind(this, this.addCategory);
+
+    return (this);
+}
+
+InnerAppController.prototype = {
+    init: function () {
+        this.categories = [
+                { id: 1, name: 'First Category', todoCount: 1 }
+        ];
+
+        this.todos = [
+                { id: 1, text: 'First Task', categoryId: 1, done: false }
+        ];
+
+        this.menuService.updateEntries(this.categories);
+        this.category = this.categories[0];
+
+        this.menuService.registerMenuChooseCallback(angular.bind(this, this.switchCategory));
+
+        this.updateTodos();
+    },
+
+    switchCategory: function (category) {
+        this.category = category;
+        this.updateTodos();
+        this.scope.$apply();
+    },
+
+    updateTodos: function () {
+        var todosFromCategory = this.filter('filter')(this.todos, { categoryId: this.category.id });
+        this.scope.currentTodos = todosFromCategory;
+    },
+
+    addTodo: function () {
+        this.todos.push({ text: this.scope.todoText, done: false, categoryId: this.category.id });
+
+        var todosFromCategory = this.filter('filter')(this.todos, { categoryId: this.category.id });
+        var todosFromCategoryCount = todosFromCategory.length;
+
+        this.category.todoCount = todosFromCategoryCount;
+        this.scope.todoText = '';
+
+        this.scope.currentTodos = todosFromCategory;
+
+        this.menuService.updateEntries(this.categories);
+    },
+
+    addCategory: function () {
+
+        var sortedCategories = this.filter('filter')(this.categories, function (category) { return category.id; });
+        var newCategory = { id: sortedCategories[sortedCategories.length - 1].id + 1, name: this.scope.categoryText, todoCount: 0 }
+
+        this.categories.push(newCategory);
+        this.category = newCategory;
+
+        this.updateTodos();
+
+        this.menuService.updateEntries(this.categories);
+
+        this.scope.categoryText = '';
+    }
+
+    //    $scope.remaining = function () {
+    //        var count = 0;
+    //        angular.forEach($scope.currentTodos, function (todo) {
+    //            count += todo.done ? 0 : 1;
+    //        });
+    //        return count;
+    //    };
+
+    //    $scope.archive = function () {
+    //        var oldTodos = $scope.todos;
+    //        $scope.todos = [];
+    //        angular.forEach(oldTodos, function (todo) {
+    //            if (!todo.done) $scope.todos.push(todo);
+    //        });
+    //    };
+}
